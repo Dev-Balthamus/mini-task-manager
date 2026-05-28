125de4b4edeb7956d4bfb7af6db0a617317d9ba1
Francesco Balsamo <francesco7411@gmail.com>
2026-05-28 15:10:09 +0200

    Frontend Code: Added useTasksJSON Custom Hook

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
 import "./App.css";
+import { useTasksJSON } from "./assets/custom-hooks/useTasksJSON";
 import Filters from "./components/Filters";
 import TasksList from "./components/TasksList";
 
 function App() {
+  const { loading, error, data } = useTasksJSON();
+
   return (
     <div className="appContainer">
       <h1 className="appTitle">Mini Task Manager</h1>
@@ -10,7 +13,11 @@ function App() {
         New Task
       </button>
       <Filters />
-      <TasksList items={[]} />
+      <div className="tasksContainer">
+        {loading && <h2>Loading Tasks' List...</h2>}
+        {error && <h2>Error loading Tasks' List: {error.message}</h2>}
+        {data && <TasksList items={data} />}
+      </div>
     </div>
   );
 }
---
+++ frontend/src/assets/custom-hooks/useTasksJSON.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+
+export interface Task {
+  id: number;
+  title: string;
+  description: string;
+  priority: string;
+}
+
+export function useTasksJSON() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+  const [data, setData] = useState<Task[] | null>(null);
+
+  async function fetchTasksJSON() {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch("http://localhost:3000/api/tasks");
+      const tasks = await response.json();
+
+      if (response.status !== 200) {
+        setError(new Error());
+      }
+
+      setData(tasks);
+    } catch (error) {
+      setError(error as Error);
+      setData(null);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchTasksJSON();
+  }, []);
+
+  return { loading, error, data };
+}
