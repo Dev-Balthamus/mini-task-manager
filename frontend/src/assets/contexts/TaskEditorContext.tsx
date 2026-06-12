898168eb4f181e117cbd0d3ffcd53f8ca1787843
Francesco Balsamo <francesco7411@gmail.com>
2026-06-12 09:00:44 +0200

    Frontend Code: Activated TypeScript Strict Mode; refactored and fixed Filters logic and component

--- frontend/src/assets/contexts/TaskEditorContext.tsx
+++ frontend/src/assets/contexts/TaskEditorContext.tsx
@@ -1,15 +1,18 @@
 import { createContext, useContext, useMemo, useState, type ReactNode } from "react";
 import { useTasksJSON, type Task } from "../custom-hooks/useTasksJSON";
 
-export type OrderCriterion = "" | "low" | "high" | "executed" | "pending";
+export type PriorityCriterion = "" | "low" | "high";
+export type ExecutionCriterion = "" | "executed" | "pending";
 
 interface TaskContext {
   loading: boolean;
   error: Error | null;
   tasks: Task[] | null;
   reloadTasks: () => Promise<void>;
-  orderCriterion: OrderCriterion;
-  toOrderCriterion: (criterion: OrderCriterion) => void;
+  priority: PriorityCriterion;
+  toOrderByPriority: (priority: PriorityCriterion) => void;
+  execution: ExecutionCriterion;
+  toOrderByExecution: (priority: ExecutionCriterion) => void;
 }
 
 const TaskEditorContext = createContext<TaskContext | undefined>(undefined);
@@ -28,10 +31,14 @@ interface TaskEditorProps {
 
 export const TaskEditor: React.FC<TaskEditorProps> = ({ children }) => {
   const { loading, error, data, onReloadTasks } = useTasksJSON();
-  const [orderCriterion, setOrderCriterion] = useState<OrderCriterion>("");
+  const [priority, setPriority] = useState<PriorityCriterion>("");
+  const [execution, setExecution] = useState<ExecutionCriterion>("");
 
-  function toOrderCriterion(criterion: OrderCriterion) {
-    setOrderCriterion(criterion);
+  function toOrderByPriority(priority: PriorityCriterion) {
+    setPriority(priority);
+  }
+  function toOrderByExecution(execution: ExecutionCriterion) {
+    setExecution(execution);
   }
 
   const orderedTasks = useMemo(() => {
@@ -39,98 +46,67 @@ export const TaskEditor: React.FC<TaskEditorProps> = ({ children }) => {
 
     const tasksCopy = [...data];
 
-    if (orderCriterion === "low") {
-      tasksCopy.sort((a, b) => {
-        if (a.priority === "low" && b.priority === "high") {
-          return -1;
+    const priorityNumValue: Map<string, number> = new Map();
+
+    switch (priority) {
+      case "low":
+        {
+          priorityNumValue.set("low", 0);
+          priorityNumValue.set("medium", 1);
+          priorityNumValue.set("high", 2);
         }
-        if (a.priority === "low" && b.priority === "medium") {
-          return -1;
+        break;
+      case "high":
+        {
+          priorityNumValue.set("low", 2);
+          priorityNumValue.set("medium", 1);
+          priorityNumValue.set("high", 0);
         }
-        if (a.priority === "medium" && b.priority === "high") {
-          return -1;
-        }
-        if (a.priority === b.priority) {
-          if (a.id < b.id) {
-            return -1;
-          }
-          if (a.id > b.id) {
-            return 1;
-          }
-        }
-        return 0;
-      });
+        break;
+      default: {
+        priorityNumValue.set("low", 1);
+        priorityNumValue.set("medium", 1);
+        priorityNumValue.set("high", 1);
+      }
     }
 
-    if (orderCriterion === "high") {
-      tasksCopy.sort((a, b) => {
-        if (a.priority === "high" && b.priority === "low") {
-          return -1;
-        }
-        if (a.priority === "medium" && b.priority === "low") {
-          return -1;
-        }
-        if (a.priority === "high" && b.priority === "medium") {
-          return -1;
-        }
-        if (a.priority === b.priority) {
-          if (a.id < b.id) {
-            return -1;
-          }
-          if (a.id > b.id) {
-            return 1;
-          }
-        }
-        return 0;
-      });
-    }
+    tasksCopy.sort((a, b) => {
+      if (execution === "executed" || execution === "pending") {
+        const aExecutionStatus = a.executed === true ? 1 : 0;
+        const bExecutionStatus = b.executed === true ? 1 : 0;
 
-    if (orderCriterion === "executed") {
-      tasksCopy.sort((a, b) => {
-        if (a.executed === true && b.executed === false) {
-          return -1;
-        }
-        if (a.executed === false && b.executed === true) {
-          return 1;
-        }
-        if (a.executed === b.executed) {
-          if (a.id < b.id) {
-            return -1;
-          }
-          if (a.id > b.id) {
-            return 1;
-          }
-        }
-        return 0;
-      });
-    }
+        const order = execution === "executed" ? 1 : -1;
 
-    if (orderCriterion === "pending") {
-      tasksCopy.sort((a, b) => {
-        if (a.executed === false && b.executed === true) {
-          return -1;
-        }
-        if (a.executed === true && b.executed === false) {
-          return 1;
-        }
-        if (a.executed === b.executed) {
-          if (a.id < b.id) {
-            return -1;
-          }
-          if (a.id > b.id) {
-            return 1;
-          }
-        }
-        return 0;
-      });
-    }
+        if (aExecutionStatus !== bExecutionStatus) return aExecutionStatus > bExecutionStatus ? -order : order;
+      }
+
+      if (priority === "low" || priority === "high") {
+        const priorityANum = priorityNumValue.get(a.priority)!;
+        const priorityBNum = priorityNumValue.get(b.priority)!;
+
+        if (priorityANum !== priorityBNum) return priorityANum - priorityBNum;
+      }
+
+      if (a.id !== b.id) return a.id < b.id ? -1 : 1;
+
+      return 0;
+    });
 
     return tasksCopy;
-  }, [data, orderCriterion]);
+  }, [data, priority, execution]);
 
   return (
     <TaskEditorContext.Provider
-      value={{ loading, error, tasks: orderedTasks, reloadTasks: onReloadTasks, orderCriterion, toOrderCriterion }}
+      value={{
+        loading,
+        error,
+        tasks: orderedTasks,
+        reloadTasks: onReloadTasks,
+        priority,
+        toOrderByPriority,
+        execution,
+        toOrderByExecution,
+      }}
     >
       {children}
     </TaskEditorContext.Provider>
--- frontend/src/components/Filters.tsx
+++ frontend/src/components/Filters.tsx
@@ -1,38 +1,31 @@
-import { useTaskEditor } from "../assets/contexts/TaskEditorContext";
+import { type PriorityCriterion, type ExecutionCriterion, useTaskEditor } from "../assets/contexts/TaskEditorContext";
 import "./Filters.css";
 
 function Filters() {
-  const { orderCriterion, toOrderCriterion } = useTaskEditor();
+  const { priority, toOrderByPriority, execution, toOrderByExecution } = useTaskEditor();
 
-  function handleFilterChange(event: React.ChangeEvent<HTMLSelectElement>) {
-    const selectedCriterion = event.target.value as any;
-    toOrderCriterion(selectedCriterion);
+  function handlePriorityChange(event: React.ChangeEvent<HTMLSelectElement>) {
+    const selectedPriority = event.target.value as PriorityCriterion;
+    toOrderByPriority(selectedPriority);
+  }
+
+  function handleExecutionChange(event: React.ChangeEvent<HTMLSelectElement>) {
+    const selectedExecution = event.target.value as ExecutionCriterion;
+    toOrderByExecution(selectedExecution);
   }
 
   return (
     <div className="filtersContainer">
       <h5 className="filtersTitle">Filters</h5>
-      <select className="filter" value={orderCriterion} onChange={handleFilterChange}>
-        <option value="" disabled>
-          Order by Priority...
-        </option>
-        <option value="low" className={orderCriterion === "low" ? "selected" : ""}>
-          from Low to High
-        </option>
-        <option value="high" className={orderCriterion === "high" ? "selected" : ""}>
-          from High to Low
-        </option>
+      <select className="filter" value={priority} onChange={handlePriorityChange}>
+        <option value="">Order by Priority...</option>
+        <option value="low">from Low to High</option>
+        <option value="high">from High to Low</option>
       </select>
-      <select className="filter" value={orderCriterion} onChange={handleFilterChange}>
-        <option value="" disabled>
-          Filter by Execution Status...
-        </option>
-        <option value="executed" className={orderCriterion === "executed" ? "selected" : ""}>
-          Executed Tasks
-        </option>
-        <option value="pending" className={orderCriterion === "pending" ? "selected" : ""}>
-          Pending Tasks
-        </option>
+      <select className="filter" value={execution} onChange={handleExecutionChange}>
+        <option value="">Filter by Execution Status...</option>
+        <option value="executed">Executed Tasks</option>
+        <option value="pending">Pending Tasks</option>
       </select>
     </div>
   );
