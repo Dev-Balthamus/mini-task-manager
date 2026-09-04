ac6d5f2e367beb352503274b809cd1500a409360
Francesco Balsamo <tua_email_personale@esempio.com>
2026-09-04 11:30:02 +0200

    Add of Users and Auth Context, Phase 01: Added automated tests for Users Migration and Register User Endpoint

--- backend/tests/index.test.ts
+++ backend/tests/index.test.ts
@@ -5,6 +5,7 @@ import { pool } from "../src/repository/infrastructure/pool.ts";
 import "./cases/db-migration.test.ts";
 import "./cases/api-and-pool.test.ts";
 import "./cases/data-migration.test.ts";
+import "./cases/users-db-update-api-registration.test.ts";
 
 // Chiusura del Connection Pool alla fine di tutti i test
 after(async () => {
